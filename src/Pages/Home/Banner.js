b7cd8a740ce8476953b2086ba765b964caf71856
juelhossain <juelhossainnn@gmail.com>
2022-10-20 04:22:45 +0600

    banner completed

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,33 +1,29 @@
-
-import { Box, Container, Stack, Typography } from '@mui/material';
-import React from 'react';
-import bannerimage from '../../assets/screw image.webp'
+import { Box, Container } from "@mui/material";
+import React from "react";
+import bannerimage from "../../assets/screw image.webp";
 
 const Banner = () => {
-    return (
-      <Container maxWidth={"xl"}>
-        <Box sx={{ display: "flex",}}>
-          <Stack
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              ml:4,
-              flex: 1,
-            }}
-          >
-            <Typography variant="h2">Screw-Inc.</Typography>
-            <Typography variant="h5">We Manufacture the Futuristic smart Tools Which We Are Mad to buy...</Typography>
-          </Stack>
-          <Box sx={{flex:1}}>
-            <img
-              className="w-full h-full object-cover m-2"
-              src={bannerimage}
-              alt="hero-banner"
-            />
-          </Box>
+  return (
+    <Container maxWidth={"xl"} className="border mt-5 shadow">
+      <Box className="flex w-full flex-col sm:flex-row py-2 gap-5">
+        <Box className="flex flex-col flex-1 justify-center items-center text-center sm:items-start sm:text-left gap-2 sm:ml-5 ml-3 md:ml-8 lg:ml-10 ">
+          <p className="text-6xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-semibold">
+            Screw-Inc.
+          </p>
+          <p className="text-2xl sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-semibold">
+            We Manufacture the Futuristic smart Tools Which We Are Mad to buy...
+          </p>
         </Box>
-      </Container>
-    );
+        <Box className="flex-1">
+          <img
+            className="w-full h-full object-cover "
+            src={bannerimage}
+            alt="hero-banner"
+          />
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default Banner;
