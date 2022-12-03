16b3bcd6973cfcadc0a65fe25667a2c22d666501
juelhossain <juelhossainnn@gmail.com>
2022-12-03 15:17:37 +0600

    server url changed

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { createTheme } from "@mui/material/styles";
-import { ThemeProvider } from 'styled-components';
-import axios from 'axios';
+import { ThemeProvider } from "styled-components";
+import axios from "axios";
 import { QueryClient, QueryClientProvider } from "react-query";
- 
-// setting default base url of axios 
-axios.defaults.baseURL = "https://screw-inc.herokuapp.com";
 
-// setting a default auth header to axios 
-axios.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem("accessToken")}`;
+// setting default base url of axios
+axios.defaults.baseURL = "https://screw-inc-server.onrender.com";
 
-// creating query client of react query 
+// setting a default auth header to axios
+axios.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem(
+  "accessToken"
+)}`;
+
+// creating query client of react query
 const queryClient = new QueryClient();
 
 // creating theme of the app
@@ -51,17 +53,17 @@ const theme = createTheme({
     borderRadius: 4,
   },
 });
- 
+
 // getting the root of the index.html and injecting our whole app on index html with react
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <QueryClientProvider client={queryClient}>
-            <App />
-          </QueryClientProvider>
-        </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
